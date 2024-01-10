5d9c0eae1f07876c5c468589c999509fc7bd0e1c
soyeon126 <97427610+soyeon126@users.noreply.github.com>
2024-01-10 11:21:19 +0900

    add title and fix date list composable

--- app/src/main/java/com/example/nailed_it/screen/calendar/MainCalendarScreen.kt
+++ app/src/main/java/com/example/nailed_it/screen/calendar/MainCalendarScreen.kt
@@ -1,13 +1,8 @@
 package com.example.nailed_it.screen.calendar
 
-import DayOfWeek
-import DayOfWeek.FRI
-import DayOfWeek.MON
 import DayOfWeek.SAT
 import DayOfWeek.SUN
 import DayOfWeek.THU
-import DayOfWeek.TUE
-import DayOfWeek.WED
 import DayOfWeek.dayOfWeekList
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -27,44 +23,45 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class MainCalendar {
+@Composable
+fun MainCalendar() {
+    Column {
+        MonthTitle(month = "2021년 8월")
+        DateList(lastDate = 31, firstDayOfWeek = THU)
+    }
 }
 
 @Composable
-fun CalendarList(lastDate: Int, firstDayOfWeek: String) {
+fun MonthTitle(month: String) {
+    Text(
+        text = month,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(10.dp)
+    )
+}
+
+@Composable
+fun DateList(lastDate: Int, firstDayOfWeek: String) {
     val firstDay = dayOfWeekList.indexOf(firstDayOfWeek)
+
     Column(modifier = Modifier.padding(10.dp)) {
         DayOfWeekItem()
-        //첫째주
-        Row() {
-            //첫 요일 전까지 빈 박스 생성
-            for (i in 1..firstDay) {
-                Spacer(modifier = Modifier.size(60.dp, 0.dp))
+
+        var currentDay = 1
+
+        repeat(6) { // 최대 6주까지만 표시
+            Row {
+                repeat(7) {
+                    if ((it < firstDay && currentDay == 1) || currentDay > lastDate) {
+                        Spacer(modifier = Modifier.size(60.dp, 0.dp))
+                    } else {
+                        CalendarItem(currentDay)
+                        currentDay++
+                    }
+                }
             }
-            //첫 주는 생성, 날짜 넘으면 넘김
-            for (i in 1..7 - firstDay) {
-                CalendarItem(i)
-            }
-        }
-        //둘째주
-        Row() {
-            for (i in 8 - firstDay..14 - firstDay) {
-                CalendarItem(i)
-            }
-        }
-        //셋째주
-        Row() {
-            for (i in 15 - firstDay..21 - firstDay) {
-                CalendarItem(i)
-            }
-        }
-        //넷째주
-        Row() {
-            for (i in 22 - firstDay..28 - firstDay) { CalendarItem(i) }
-        }
-        //다섯째주
-        Row() {
-            for (i in 29 - firstDay..lastDate) { CalendarItem(i) }
         }
     }
 }
@@ -74,23 +71,19 @@ fun DayOfWeekItem() {
     val modifierForText = Modifier
         .size(50.dp)
     Row() {
-        Text(text = MON, textAlign = TextAlign.Center, modifier = modifierForText)
-        Text(text = TUE, textAlign = TextAlign.Center, modifier = modifierForText)
-        Text(text = WED, textAlign = TextAlign.Center, modifier = modifierForText)
-        Text(text = THU, textAlign = TextAlign.Center, modifier = modifierForText)
-        Text(text = FRI, textAlign = TextAlign.Center, modifier = modifierForText)
-        Text(
-            text = SAT,
-            textAlign = TextAlign.Center,
-            modifier = modifierForText,
-            color = Color.Red
-        )
-        Text(
-            text = SUN,
-            textAlign = TextAlign.Center,
-            modifier = modifierForText,
-            color = Color.Red
-        )
+        dayOfWeekList.forEach {
+            if (it != SUN && it != SAT) Text(
+                text = it,
+                textAlign = TextAlign.Center,
+                modifier = modifierForText
+            )
+            else Text(
+                text = it,
+                textAlign = TextAlign.Center,
+                modifier = modifierForText,
+                color = Color.Red
+            )
+        }
     }
 }
 
@@ -114,5 +107,5 @@ fun CalendarItem(date: Int) {
 @Preview
 @Composable
 fun CalendarItemPreview() {
-    CalendarList(31, WED)
+    MainCalendar()
 }
